41278528ebd87777abdb91a00e248e3a0704fcf1
Matheus <matheussiqueiraalmeida7@gmail.com>
2025-02-27 19:34:51 -0400

    exemples.c

---
+++ variables/exemples.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+//college student
+    int studentID = 15;
+    int studentAge = 23;
+    float studentFee = 75.25;
+    char studentGrade = 'B';
+
+    printf("Student id: %d\n", studentID);
+    printf("Student age: %d\n", studentAge);
+    printf("Student fee: %f\n", studentFee);
+    printf("Student grade: %c\n", studentGrade);
+
+//calculate the Area of a Rectangle
+    int lenght = 4;
+    int width = 6;
+    int area;
+
+    area = lenght * width;
+
+    printf("Length is: %d\n", lenght);
+    printf("Width is: %d\n", width);
+    printf("Area of the rectange is: %d\n", area);
+
+}
